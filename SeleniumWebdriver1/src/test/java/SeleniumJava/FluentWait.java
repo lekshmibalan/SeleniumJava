4720ce93dcc8dc81d31389ec21f654c7d410c95a
lekshmi <lekshmi@kranadmin>
2025-08-11 12:21:28 +0530

    fluentwait

---
+++ SeleniumWebdriver1/src/test/java/SeleniumJava/FluentWait.java
@@ -0,0 +1,40 @@
+package SeleniumJava;
+
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import com.google.common.base.Function;
+
+public class FluentWait {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+
+       //Fluent wait declaration
+		Wait<WebDriver> mywait = new FluentWait<WebDriver>(driver)
+			    .withTimeout(Duration.ofSeconds(30))
+			    .pollingEvery(Duration.ofSeconds(5))
+			    .ignoring(NoSuchElementException.class);
+		
+	    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		driver.manage().window().maximize(); 
+		 WebElement txtUsername = mywait.until(new Function<WebDriver, WebElement>() {
+		     public WebElement apply(WebDriver driver) {
+		       return driver.findElement(By.xpath("//input[@placeholder='Username']"));
+		     }
+		   });
+           txtUsername.sendKeys("Admin");
+		
+		//driver.close();
+		}
+		}
+
+	
+
+
