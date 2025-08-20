722282e48d478e4bf08b3a0f11281aaaa432c583
lekshmi <lekshmi@kranadmin>
2025-08-20 15:52:32 +0530

    dependencymethods

---
+++ SeleniumWebdriver1/src/test/java/DependencyMethods/DependsOnMethods.java
@@ -0,0 +1,30 @@
+package DependencyMethods;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class DependsOnMethods {
+    @Test(priority=1)
+	void openapp()
+	{
+    	Assert.assertTrue(true);
+	}
+    @Test(priority=2,dependsOnMethods= {"openapp"})
+	void login()
+	{
+		Assert.assertTrue(false);
+	}
+    @Test(priority=3,dependsOnMethods= {"login"})
+   	void search()
+   	{
+    	Assert.assertTrue(true);
+   	}
+    @Test(priority=4,dependsOnMethods= {"login"})
+   	void advsearch()
+   	{
+    	Assert.assertTrue(true);
+   	} 
+    @Test(priority=5,dependsOnMethods= {"login"})
+   	void logout()
+   	{
+    	Assert.assertTrue(true);
+   	}
+}
