5e19d9942240c533f313898677201ab670e6fa08
lekshmi <lekshmi@kranadmin>
2025-09-22 12:14:03 +0530

    mt

--- MakeMyTrip/src/main/java/makemytrippages/LoginPage.java
+++ MakeMyTrip/src/main/java/makemytrippages/LoginPage.java
@@ -1,5 +1,36 @@
 package makemytrippages;
 
-public class LoginPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import makemytripbase.BaseTest;
+
+public class LoginPage extends BaseTest {
+	
+@FindBy(xpath="//span[text()='Login or']")
+WebElement login;
+
+@FindBy(xpath="//input[@placeholder='Enter Mobile Number']")
+WebElement EnterMobile;
+
+@FindBy(xpath="//span[text()='Continue']")
+WebElement Continuebtn;
+
+public LoginPage() {
+	super();
+	PageFactory.initElements(driver, this);
+}
+
+public void login() {
+	login.click();
+}
+ 
+public void EnterMobile() {
+	 EnterMobile.sendKeys("9876543210");
+ }
+
+ public void ClickContinue() {
+	 Continuebtn.click();
+ }
 }
--- MakeMyTrip/src/test/java/makemytriptestcases/LoginPageTest.java
+++ MakeMyTrip/src/test/java/makemytriptestcases/LoginPageTest.java
@@ -1,5 +1,36 @@
 package makemytriptestcases;
 
-public class LoginPageTest {
+import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.Status;
+
+import makemytripbase.BaseTest;
+import makemytrippages.LoginPage;
+
+public class LoginPageTest extends BaseTest{
+
+	@Test
+	public void testLoginPage() {
+		
+	test =extent.createTest("Login to Book Flight")	;
+	LoginPage login =new LoginPage();
+	
+	login.login();
+	test.log(Status.INFO, "Clicked on Login link");
+	
+	login.EnterMobile();
+	test.log(Status.INFO,"Entered Mobile Number");
+	
+	login.ClickContinue();
+	test.log(Status.INFO, "Clicked on Continue Button");
+	
+	
+	
+	
+	
+	
+	
+	}
+	
+	
 }
